5bdc6063b571b87f30e5ca517975b3f200456081
Julien Boyer <juliien.boyer49@orange.fr>
2017-12-14 11:12:02 +0100

    Fix Semantic behaviors UI

--- src/components/ComponentDispatcher.js
+++ src/components/ComponentDispatcher.js
@@ -34,7 +34,7 @@ const Dispatcher = (roles, userRequired) =>
         }
 
         return (
-          <Grid container-fluid columns={1}>
+          <Grid columns={1}>
             <Grid.Column>
               <DynamicComponent userSession={this.props.userSession} />
             </Grid.Column>
